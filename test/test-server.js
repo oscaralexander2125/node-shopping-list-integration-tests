9977222f1c4f0827210d32d474bb86a5f7c0b0cf
Oscar A. Reyes <oscarreyes2194@gmail.com>
2019-01-03 18:35:10 -0600

    answer to integration testing with chait assignmen

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,78 @@ describe("Shopping List", function() {
     );
   });
 });
+
+describe('Recipes', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('it should list recipes on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      expect(res.body.length).to.be.above(0);
+      res.body.forEach(function(item) {
+        expect(item).to.be.a('object');
+        expect(item).to.have.all.keys('name', 'id', 'ingredients');
+      });
+    });
+  });
+  it('it should add recipe on POST', function() {
+    const newItem= {
+      name: 'coke',
+      ingredients: [
+        'sugar', 'water'
+      ],
+    };
+    return chai.request(app)
+    .post('/recipes')
+    .send(newItem)
+    .then(function(res) {
+      expect(res).to.have.status(201);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.include.keys('name','id','ingredients');
+      expect(res.body.id).to.not.equal(null);
+      expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+    });
+  });
+  it('it should update recipe on PUT', function() {
+    const updateDatas= {
+      name: 'sprite',
+      ingredients: ['less sugar', 'water']
+    };
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      updateDatas.id=res.body[0].id;
+      return chai.request(app)
+      .put(`/recipes/${updateDatas.id}`)
+      .send(updateDatas)
+    })
+    .then(function(res) {
+      expect(res).to.have.status(204);
+      /*expect(res).to.be.json;
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.deep.equal(updateDatas);*/ // why wont this work here but it does in the shoppin list example
+    });
+  });
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+      .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res) {
+      expect(res).to.have.status(204);
+    });
+  });
+});
